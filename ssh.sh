15187a0c44f3e3ef9bdf069e5ea61d97468e868b
LiHao <2223128221@qq.com>
2024-11-28 10:51:11 +0800

    [fix]修复xftp无法连接bug

--- ssh.sh
+++ ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 检查当前执行的命令是否是 scp 或 sftp，或者其他不需要触发监控的命令
-if [[ "$SSH_ORIGINAL_COMMAND" =~ ^scp.* || "$SSH_ORIGINAL_COMMAND" =~ ^sftp.* ]]; then
+if [[ "$SSH_ORIGINAL_COMMAND" =~ ^scp.* || "$SSH_ORIGINAL_COMMAND" =~ sftp.* ]]; then
     # 如果是 scp 或 sftp，则直接执行原命令，不启动 Agith
     exec $SSH_ORIGINAL_COMMAND
 else
